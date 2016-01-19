4668d88aae9ab871ca5333831b605973b13d55ca
Jason Amerine <amerine.19@gmail.com>
2016-01-19 18:23:55 -0500

    Assignment 37 Authentication Signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  post 'users/confirm' => 'users#confirm'
+  
   get 'about' => 'welcome#about'
+
+
 
   root 'welcome#index'
 end
